f5fc6b42eb19a3a988c9ad91638587fbd6199b32
fawad171 <fawad171@gmail.com>
2023-01-10 18:07:46 -0500

    new project

---
+++ action.js
@@ -0,0 +1,33 @@
+let num = 0;
+function increment() {
+    const countElem = document.getElementById('count');
+    countElem.innerText = ++num;
+}
+function decrement() {
+    const countElem = document.getElementById('count');
+    countElem.innerText = --num;
+}
+function reset() {
+    const countElem = document.getElementById('count');
+    num = 0;
+    countElem.innerText = 0;
+}
+
+function modifyNum(number) {
+    const countElem = document.getElementById('count');
+    num += number;
+    countElem.innerText = num;
+}
+
+function fizzbuzz() {
+    console.log(document.querySelector('#fizzbuzz-input').value);
+    //you'll want to look up
+    //javascript loops (either while, or for will work well here)
+    //conditionals (if, elseif, else) or switch statements
+    //modulo operator will also come handy here.
+}
+
+function calculator(value) {
+}
+let name='Ahmad'
+console.log(name);
---
+++ events.js
@@ -0,0 +1,5 @@
+function greet() {
+
+    console.log('this is ny code');
+}
+greet();
